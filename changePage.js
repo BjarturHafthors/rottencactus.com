b41bd2f92c1d2d10d3644ccded3e8f936fe63980
SwagMal <bjarturhafthors@hotmail.com>
2016-04-26 21:30:23 +0000

    Update on look, language, content and structure!

---
+++ changePage.js
@@ -0,0 +1,11 @@
+function goToSonder() {
+	document.location.href = "sonder.html";
+}
+
+function goToSimmiBird() {
+	document.location.href = "simmibird.html";
+}
+
+function goToIndex() {
+	document.location.href = "index.html";
+}
